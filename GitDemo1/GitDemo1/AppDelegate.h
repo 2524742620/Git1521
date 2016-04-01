806e626316c45af605422d60c76f4be18cc63101
qianfeng <qianfeng@qianfeng11111.local>
2016-04-01 14:50:48 +0800

    第一次创建项

---
+++ GitDemo1/GitDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo1
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 王娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
